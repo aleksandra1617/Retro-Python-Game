ca831f851f41021d20de767ec0ccfe2e05a21e66
Aleksandra Petkova <a.petkova1617@gmail.com>
2019-04-06 22:43:02 +0100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 import random
 import pygame
+import time
 
 from Snake import Snake
 import GUI
@@ -9,10 +10,6 @@ pygame.init()
 width = 1080
 height = 720
 
-# Colours
-white = (255, 255, 255)
-black = (0, 0, 0)
-
 run = True
 
 # Window
@@ -21,7 +18,6 @@ pygame.display.set_caption("Snake")
 
 # Time keeping in pygame
 clock = pygame.time.Clock()
-
 
 # Get key input (QUIT, KEY DOWN, LEFT MOUSE CLICK, etc)
 def get_key_entered(player):
@@ -47,24 +43,33 @@ def randomize_apple_pos(app_width, app_height):
 
     return rand_x, rand_y
 
+def wait_for(miliseconds):
+    clock = pygame.time.Clock()
+    clock.tick()
+    current_time = clock.get_time()
+    print(current_time)
+    clock.tick()
 
 def main():
     view_width = 1000
     view_height = 600
     score = 0
+    lives = 3
+    liveTaken = False
 
     # Construct a snake
-    snake = Snake(400, 400, 3, 3, 15, black)
+    snake = Snake(400, 400, 3, 3, 15, (0,0,0))
 
     # Apple
     app_width = 10
     app_height = 10
 
     rand_x, rand_y = randomize_apple_pos(app_width, app_height)
+
     # Game loop
     while run:
         # Draw background, needs to happen every frame
-        window.fill(white)
+        window.fill((255, 255, 255))
 
         # Calculate game view rectangle starting position
         start_x = (width - view_width)/2
@@ -90,8 +95,21 @@ def main():
             # Update score
             score += 5
 
-        # Displays the score
-        GUI.display_text(window, "Score: " + str(score), 50, (0, 0, 0), [10, 10])
+        snakeX = snake.segments.head.rect.x
+        snakeY = snake.segments.head.rect.y
+
+        collide_top_left = game_border.collidepoint(snakeX, snakeY)
+        collide_top_right = game_border.collidepoint(snakeX+snake.size, snakeY+snake.size)
+
+        if not (collide_top_left and collide_top_right):
+            lives -= 1
+            liveTaken = True
+
+        wait_for(1000)
+
+        # Displays TEXT
+        GUI.display_text(window, "Score: " + str(score), 50, (0, 0, 0), [40, 10])
+        GUI.display_text(window, "Lives: " + str(lives), 50, (0, 0, 0), [910, 10])
 
         # Displays the buffered data
         pygame.display.update()
