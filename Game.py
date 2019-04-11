a54c2cfa6b17ee5fffde444c54da041a79c370c2
Aleksandra Petkova <a.petkova1617@gmail.com>
2019-04-11 21:12:18 +0100

    Loosing lives and snake invincibility

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 import random
 import pygame
 import time
+import math
 
 from Snake import Snake
 import GUI
@@ -35,51 +36,60 @@ def get_key_entered(player):
 
 
 # Randomly generates an apple
-def randomize_apple_pos(app_width, app_height):
+def randomize_apple_pos(app_width, app_height, game_view):
     global width, height  # window dimensions
 
-    rand_x = random.randint(0, width - app_height)
-    rand_y = random.randint(0, height - app_width)
+    rand_x = random.randint(game_view[0], game_view[2] - app_height)
+    rand_y = random.randint(game_view[1], game_view[3] - app_width)
 
     return rand_x, rand_y
 
-def wait_for(miliseconds):
-    clock = pygame.time.Clock()
-    clock.tick()
-    current_time = clock.get_time()
-    print(current_time)
-    clock.tick()
+
+def wait_for(start_time, curr_time):
+    elapsed_time = curr_time - start_time
+
+    #if elapsed_time > seconds:
+        #return True
+
 
 def main():
+    start_time = time.time()
+    timer_start = 0
+
     view_width = 1000
     view_height = 600
+
+    # Calculate game view rectangle starting position
+    start_x = (width - view_width) / 2
+    start_y = (height - view_height) / 2
+
+    game_view_data = (start_x, start_y, view_width, view_height)
+
     score = 0
     lives = 3
-    liveTaken = False
+    live_taken = False
 
     # Construct a snake
-    snake = Snake(400, 400, 3, 3, 15, (0,0,0))
+    snake = Snake(400, 400, 3, 3, 15, (0, 0, 0))
 
     # Apple
     app_width = 10
     app_height = 10
 
-    rand_x, rand_y = randomize_apple_pos(app_width, app_height)
+    rand_x, rand_y = randomize_apple_pos(app_width, app_height, game_view_data)
 
     # Game loop
     while run:
+        curr_time = time.time()
+
         # Draw background, needs to happen every frame
         window.fill((255, 255, 255))
-
-        # Calculate game view rectangle starting position
-        start_x = (width - view_width)/2
-        start_y = (height - view_height)/2
 
         # Player
         get_key_entered(snake)
 
-        game_view = pygame.draw.rect(window, (98, 244, 66), (start_x, start_y, view_width, view_height))
-        game_border = pygame.draw.rect(window, (3, 132, 36), (start_x, start_y, view_width, view_height), 4)
+        game_view = pygame.draw.rect(window, (98, 244, 66), game_view_data)
+        game_border = pygame.draw.rect(window, (3, 132, 36), game_view_data, 4)
 
         snake.draw_snake(window)
         snake.update_position()
@@ -90,7 +100,7 @@ def main():
         # If the apple was eaten
         if snake.eat(apple):
             # Spawn a new apple
-            rand_x, rand_y = randomize_apple_pos(app_width, app_height)
+            rand_x, rand_y = randomize_apple_pos(app_width, app_height, game_view_data)
 
             # Update score
             score += 5
@@ -98,14 +108,21 @@ def main():
         snakeX = snake.segments.head.rect.x
         snakeY = snake.segments.head.rect.y
 
-        collide_top_left = game_border.collidepoint(snakeX, snakeY)
-        collide_top_right = game_border.collidepoint(snakeX+snake.size, snakeY+snake.size)
+        # The top left and top right corners of the snake head.
+        # Those exact points create the exact boundary needed and at the same time save computation.
+        point1 = (snakeX, snakeY)
+        point2 = (snakeX+snake.size, snakeY+snake.size)
 
-        if not (collide_top_left and collide_top_right):
+        not_collide_top_left = not game_border.collidepoint(point1)
+        not_collide_top_right = not game_border.collidepoint(point2)
+
+        if not_collide_top_left and not_collide_top_right and not live_taken:
+            timer_start = curr_time
             lives -= 1
-            liveTaken = True
+            live_taken = True
 
-        wait_for(1000)
+        if round(curr_time - timer_start, 0) == 5:
+            live_taken = False
 
         # Displays TEXT
         GUI.display_text(window, "Score: " + str(score), 50, (0, 0, 0), [40, 10])
